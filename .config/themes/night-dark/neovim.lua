24d8af1a87555dd114b59005e286771b48dc24b0
bibin <bibinbiju482@gmail.com>
2026-01-25 02:14:44 +0530

    new theme

---
+++ .config/themes/night-dark/neovim.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		opts = {
+			style = "night",
+			transparent = false,
+
+			styles = {
+				sidebars = "dark",
+				floats = "dark",
+			},
+
+			on_colors = function(colors)
+				colors.bg = "#000000"
+				colors.bg_dark = "#000000"
+				colors.bg_float = "#000000"
+				colors.bg_sidebar = "#000000"
+			end,
+
+			on_highlights = function(hlc)
+				hlc.Normal = { bg = "#000000" }
+				hlc.NormalFloat = { bg = "#000000" }
+				hlc.FloatBorder = { bg = "#000000" }
+			end,
+		},
+	},
+
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight-night",
+		},
+	},
+}
