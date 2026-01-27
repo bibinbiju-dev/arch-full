ec979b79f7f7e5af0ec6558764f1e59d4c5b2e04
bibin <bibinbiju482@gmail.com>
2026-01-27 10:52:35 +0530

    dracula-nvim

--- .config/themes/dracula/neovim.lua
+++ .config/themes/dracula/neovim.lua
@@ -1,18 +1,20 @@
 return {
-	{
-		"Mofiqul/dracula.nvim",
-		config = function()
-			require("dracula").setup({
-				italic_comment = true,
-				transparent_bg = true,
-			})
-		end,
-	},
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	config = function()
+	-- 		require("dracula").setup({
+	-- 			italic_comment = true,
+	-- 			transparent_bg = false,
+	-- 		})
+	-- 	end,
+	-- },
 
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "dracula",
+			colorscheme_bg = "dark",
+			italic_comment = true,
 		},
 	},
 }
